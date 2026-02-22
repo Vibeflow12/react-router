080901a217892bdc61c4a60af560b5f6953472e8
prashanth <kasamsaimaniprashanth@gmail.com>
2026-02-22 22:51:23 +0530

    client loader

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,15 +1,18 @@
 import type { Route } from "./+types/post";
 
-export async function loader({ params }: Route.LoaderArgs) {
+export async function clientLoader({ params }: Route.LoaderArgs) {
     const postId = params.postId;
-    return { postId }
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    return await res.json()
 }
 export async function action() { }
 
 export default function Post({ loaderData }: Route.ComponentProps) {
     return (
         <div>
-            <p>Post ID : {loaderData.postId}</p>
+            <p>Title: {loaderData.title}</p>
+            <p>Body : {loaderData.body}</p>
+            <p>Id: {loaderData.id}</p>
         </div>
     )
 }
